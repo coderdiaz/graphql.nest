c88c95951cfe7ba5b0841b5b094ffe293862d32b
Javier Diaz <hello@coderdiaz.me>
2019-05-15 23:24:05 -0500

    feat: imported task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { StatusModule } from './status/status.module';
+import { TasksModule } from './tasks/tasks.module';
 
 const debugMode: boolean = process.env.NODE_ENV === 'development';
 
 @Module({
   imports: [
     StatusModule,
+    TasksModule,
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       debug: debugMode,
